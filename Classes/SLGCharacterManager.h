027f11a3f42df0836adfc3e6876ad13a32b91fbd
jj918160 <446413497@qq.com>
2016-04-02 10:09:45 +0800

    4-2

---
+++ Classes/SLGCharacterManager.h
@@ -0,0 +1,14 @@
+//
+//  SLGCharacterManager.h
+//  RPG_LYBQ
+//
+//  Created by mac on 16-4-2.
+//
+//
+
+#ifndef __RPG_LYBQ__SLGCharacterManager__
+#define __RPG_LYBQ__SLGCharacterManager__
+
+#include <stdio.h>
+
+#endif /* defined(__RPG_LYBQ__SLGCharacterManager__) */
